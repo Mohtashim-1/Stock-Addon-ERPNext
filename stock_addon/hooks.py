61cc33fadbae1f7630c7365b2af3b9f5e61445e7
Mohtashim-1 <shoaibmohtashin973@gmail.com>
2025-06-16 19:05:05 +0500

    Show and make mandatory if the naming_series matches any allowed series

--- stock_addon/hooks.py
+++ stock_addon/hooks.py
@@ -29,7 +29,8 @@ app_license = "mit"
 
 # include js in doctype views
 doctype_js = {
-    "Item" : "public/js/item.js"
+    "Item" : "public/js/item.js",
+    "Purchase Receipt" : "public/js/purchase_receipt.js "
     }
 # doctype_list_js = {"doctype" : "public/js/doctype_list.js"}
 # doctype_tree_js = {"doctype" : "public/js/doctype_tree.js"}
--- stock_addon/public/js/purchase_receipt.js
+++ stock_addon/public/js/purchase_receipt.js
@@ -1,21 +1,34 @@
 frappe.ui.form.on('Purchase Receipt', {
     refresh: function(frm) {
-        // List of allowed series
-        const allowed_series = [
-            "PR-FABRIC-.###.-.YY.",
-            "PR-IMP MATERIAL-.###.-.YY.",
-            "PR-STOCK LOT-.###.-.YY.",
-            "PR-TOWEL-.###.-.YY."
-        ];
-
-        // Hide and make not mandatory by default
-        frm.set_df_property('custom_create_landed_cost_', 'hidden', 1);
-        frm.set_df_property('custom_create_landed_cost_', 'reqd', 0);
-
-        // Show and make mandatory if the naming_series matches any allowed series
-        if (allowed_series.includes(frm.doc.naming_series)) {
-            frm.set_df_property('custom_create_landed_cost_', 'hidden', 0);
-            frm.set_df_property('custom_create_landed_cost_', 'reqd', 1);
-        }
+        toggle_landed_cost_field(frm);
+    },
+    naming_series: function(frm) {
+        toggle_landed_cost_field(frm);
+    },
+    onload_post_render: function(frm) {
+        toggle_landed_cost_field(frm);
+    },
+    supplier_delivery_note: function(frm) {
+        toggle_landed_cost_field(frm);
     }
 });
+
+function toggle_landed_cost_field(frm) {
+    frappe.msgprint(frm.doc.naming_series);
+    const allowed_series = [
+        "PR-FABRIC-.###.-.YY.",
+        "PR-IMP MATERIAL-.###.-.YY.",
+        "PR-STOCK LOT-.###.-.YY.",
+        "PR-TOWEL-.###.-.YY."
+    ];
+
+    // Hide and make not mandatory by default
+    frm.set_df_property('custom_create_landed_cost_', 'hidden', 1);
+    frm.set_df_property('custom_create_landeud_cost_', 'reqd', 0);
+
+    // Show and make mandatory if the naming_series matches any allowed series
+    if (allowed_series.includes(frm.doc.naming_series)) {
+        frm.set_df_property('custom_create_landed_cost_', 'hidden', 0);
+        frm.set_df_property('custom_create_landed_cost_', 'reqd', 1);
+    }
+}
