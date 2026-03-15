6d7b60acc5ee196e7bbce72ad2bb8b74d9fc439e
Mohtashim-1 <shoaibmohtashim973@gmail.com>
2026-03-15 20:18:47 +0500

    chore: update in dashboard

--- stock_addon/stock_addon/doctype/purchase_invoice/purchase_invoice_dashboard.py
+++ stock_addon/stock_addon/doctype/purchase_invoice/purchase_invoice_dashboard.py
@@ -1,29 +1,53 @@
 from frappe import _
 
-def get_data(data=None):
-    return {
-        "fieldname": "name",  # <-- Correct! Or just remove this line.
-        "non_standard_fieldnames": {
-            "Journal Entry": "reference_name",
-            "Payment Entry": "reference_name",
-            "Payment Request": "reference_name",
-            "Landed Cost Voucher": "receipt_document",
-            "Purchase Invoice": "return_against",
-            "Auto Repeat": "reference_document",
-        },
-        "internal_links": {
-            "Purchase Order": ["items", "purchase_order"],
-            "Purchase Receipt": ["items", "purchase_receipt"],
-            "Landed Cost Voucher": "custom_landed_cost_voucher_reference",
-        },
-        "transactions": [
-            {"label": _("Payment"), "items": ["Payment Entry", "Payment Request", "Journal Entry"]},
-            {
-                "label": _("Reference"),
-                "items": ["Purchase Order", "Purchase Receipt", "Asset", "Landed Cost Voucher"],
-            },
-            {"label": _("Returns"), "items": ["Purchase Invoice"]},
-            {"label": _("Subscription"), "items": ["Auto Repeat"]},
-        ],
-    }
 
+def get_data(data=None):
+	data = data or {}
+
+	non_standard_fieldnames = data.get("non_standard_fieldnames", {})
+	non_standard_fieldnames.update(
+		{
+			"Journal Entry": "reference_name",
+			"Payment Entry": "reference_name",
+			"Payment Request": "reference_name",
+			"Landed Cost Voucher": "receipt_document",
+			"Purchase Invoice": "return_against",
+			"Auto Repeat": "reference_document",
+			"Service Billing": "purchase_invoice",
+			"Meal Form": "purchase_invoice",
+		}
+	)
+
+	internal_links = data.get("internal_links", {})
+	internal_links.update(
+		{
+			"Purchase Order": ["items", "purchase_order"],
+			"Purchase Receipt": ["items", "purchase_receipt"],
+			"Landed Cost Voucher": "custom_landed_cost_voucher_reference",
+		}
+	)
+
+	transactions = data.get("transactions", [])
+	transactions.extend(
+		[
+			{"label": _("Payment"), "items": ["Payment Entry", "Payment Request", "Journal Entry"]},
+			{
+				"label": _("Reference"),
+				"items": ["Purchase Order", "Purchase Receipt", "Asset", "Landed Cost Voucher"],
+			},
+			{"label": _("Returns"), "items": ["Purchase Invoice"]},
+			{"label": _("Subscription"), "items": ["Auto Repeat"]},
+			{"label": _("Service Billing"), "items": ["Service Billing", "Meal Form"]},
+		]
+	)
+
+	data.update(
+		{
+			"fieldname": data.get("fieldname") or "name",
+			"non_standard_fieldnames": non_standard_fieldnames,
+			"internal_links": internal_links,
+			"transactions": transactions,
+		}
+	)
+
+	return data
